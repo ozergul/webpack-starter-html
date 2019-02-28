5de21cd73bbc5e4d03bd627c935e478551d0a381
Özer <ozergul@yandex.com>
2019-02-28 17:25:38 +0300

    Fonts example added

--- config/common.config.js
+++ config/common.config.js
@@ -134,7 +134,8 @@ module.exports = {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
         loader: "url-loader",
         options: {
-          limit: 1
+          limit: 1,
+          name: "./fonts/[name].[ext]"
         }
       },
       // Load images
